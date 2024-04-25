3d32b95b53ff56266e0809a434281d84f57b2adb
xinyang20--XM <15850645317@163.com>
2024-04-25 15:39:02 +0800

    Updata directory:20240419

--- 20240419/Calendar/src/CalendarFrame.java
+++ 20240419/Calendar/src/CalendarFrame.java
@@ -1,4 +1,3 @@
-import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -7,13 +6,14 @@ public class CalendarFrame extends JFrame implements ActionListener{
     JLabel labelDay[]=new JLabel[42];
     JButton titleName[]=new JButton[7];
     String name[]={"日","一","二","三","四","五","六"};
-    JButton nextMonth,previousMonth;
+    JButton nextMonth,previousMonth, jumpToyear;
     CalendarBean calendar;
     JLabel showMessage=new JLabel("",JLabel.CENTER);
-    JTextField input;
+    JTextField changeyear;
     int year=2011,month=2;
     public CalendarFrame(){
-//        input=new JTextField();
+        changeyear=new JTextField(5);
+        jumpToyear =new JButton("跳转");
         JPanel pCenter=new JPanel();
         pCenter.setLayout(new GridLayout(7,7));//代码1
         for(int i=0;i<7;i++){
@@ -31,15 +31,18 @@ public class CalendarFrame extends JFrame implements ActionListener{
         previousMonth=new JButton("上月");
         nextMonth.addActionListener(this);
         previousMonth.addActionListener(this);
+        jumpToyear.addActionListener(this);
         JPanel pNorth=new JPanel(),
                 pSouth=new JPanel();
         pNorth.add(previousMonth);
         pNorth.add(nextMonth);
         pSouth.add(showMessage);
+        pNorth.add(changeyear);
+        pNorth.add(jumpToyear);
         add(pCenter,BorderLayout.CENTER);
         add(pNorth,BorderLayout.NORTH);//代码3
         add(pSouth,BorderLayout.SOUTH);//代码4
-        setYearAndMonth(year,month);//???
+        setYearAndMonth(year,month);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
     public void setYearAndMonth(int y,int m){
@@ -69,6 +72,15 @@ public class CalendarFrame extends JFrame implements ActionListener{
             String day[]= calendar.getCalendar();
             for(int i=0;i<42;i++)
                 labelDay[i].setText(day[i]);
+        }
+        else if(e.getSource()==jumpToyear){
+            try {
+                int jumpYear = Integer.parseInt(changeyear.getText());
+                calendar.setYear(jumpYear);
+                setYearAndMonth(jumpYear, month);
+            }catch(NumberFormatException ex) {
+                JOptionPane.showMessageDialog(this,"请输入有效的年份","错误",JOptionPane.ERROR_MESSAGE);
+            }
         }
         showMessage.setText("日历："+calendar.getYear()+"年"+calendar.getMonth()+"月");
     }
--- teach/_240418/src/Role.java
+++ teach/_240418/src/Role.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Role {
+public class Role {
+    private String name;
+    private int blood;
+    public Role(String name,int blood){
+        this.name=name;
+        this.blood=blood;
+    }
 }
